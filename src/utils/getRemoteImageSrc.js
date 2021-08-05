00dd9c2558a87310d414fd2612d99e29cd5a37e1
Josh Zeltman <joshua.zeltman@gmail.com>
2021-08-05 14:39:41 -0300

    Updated build
    Updated remote image source to eliminate duplicate //

--- src/utils/getRemoteImageSrc.js
+++ src/utils/getRemoteImageSrc.js
@@ -1,6 +1,9 @@
 export default function getRemoteImageSrc(imagePath) {
   const { REACT_APP_PUBLIC_URI } = process.env;
   if (!imagePath) return false;
-  if (!imagePath.startsWith("http")) return `${REACT_APP_PUBLIC_URI}${imagePath}`;
+  if (!imagePath.startsWith("http")) {
+    imagePath = `${REACT_APP_PUBLIC_URI}${imagePath}`;
+    if (imagePath.includes("//content")) imagePath = imagePath.replace("//content", "/content");
+  }
   return imagePath;
 }
